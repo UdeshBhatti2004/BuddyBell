6a07342280162e0d70881134e1bca46058d5c433
Udesh <164016941+UdeshBhatti2004@users.noreply.github.com>
2026-02-18 22:29:41 +0530

    Refactor comments in auth-controller.js

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -37,10 +37,10 @@ export const registerUser = async (req, res) => {
       profilePicture: "",
     });
 
-    // 🧠 Generate JWT Token
+    
     const token = await newUser.generateToken();
 
-    // 🍪 Set cookie
+    
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,           
@@ -48,7 +48,7 @@ export const registerUser = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
-    // ✅ Push user to Stream
+    // Push user to Stream
     try {
       const user = await upsertUser({
         id: newUser._id.toString(),
